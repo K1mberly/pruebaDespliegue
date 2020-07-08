0e9cf8f0f7dd0769b2c6b4562136e1229716a450
Kimberly <kim.ruiz.g@gmail.com>
2020-07-08 10:57:55 -0500

    test

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ http.createServer(function (req, res) {
     console.log('texto cualquiera');    
     var numero = random.int(0, 100);
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Prueba de despliegue con github y node ' + numero);
+    res.end('Prueba de despliegue con github y node Kim' + numero);
     
 }).listen(3000);
+
